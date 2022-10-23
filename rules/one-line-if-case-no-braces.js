8c8b39c52944d18960dc066fc94fb9a390396f02
Joo Young <happycheongfamily@gmail.com>
2022-10-23 16:27:49 -0700

    report error for if statement with no elses and only one line

--- rules/one-line-if-case-no-braces.js
+++ rules/one-line-if-case-no-braces.js
@@ -11,6 +11,19 @@ module.exports = {
     },
     create(context) {
         return {
+            IfStatement(node) {
+                if(node.alternate === null && node.parent.type !== "IfStatement") {
+                    if(node.consequent.type === "BlockStatement" && node.consequent.body.length > 0) {
+                        // assumes that blank lines are removed by other eslint rules, so at most three lines for a if block with one line inside
+                        if(node.consequent.loc.end.line - node.consequent.loc.start.line + 1 <= 3) {
+                            context.report({
+                                node: node,
+                                messageId: "noSingleLineIfsAsBlock",
+                            })
+                        }
+                    }
+                }
+            }
         };
     }
 };
