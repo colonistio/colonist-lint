d8028ae8e88d3b6b2bb3a841cfd59a668e00181d
Joo Young <happycheongfamily@gmail.com>
2022-10-24 22:43:03 -0700

    declaring variables in ifs only works with braces

--- rules/one-line-if-case-no-braces.js
+++ rules/one-line-if-case-no-braces.js
@@ -14,25 +14,27 @@ module.exports = {
             IfStatement(node) {
                 if(node.alternate === null && node.parent.type !== "IfStatement") {
                     if(node.consequent.type === "BlockStatement" && node.consequent.body.length > 0) {
-                        // assumes that blank lines are removed by other eslint rules, so at most three lines for a if block with one line inside
-                        if(node.consequent.loc.end.line - node.consequent.loc.start.line + 1 <= 3) {
-                            context.report({
-                                node: node,
-                                messageId: "noSingleLineIfsAsBlock",
-                                fix(fixer) {
-                                    const sourceCode = context.getSourceCode();
+                        if(node.consequent.body[0].type !== "VariableDeclaration") {
+                            // assumes that blank lines are removed by other eslint rules, so at most three lines for a if block with one line inside
+                            if(node.consequent.loc.end.line - node.consequent.loc.start.line + 1 <= 3) {
+                                context.report({
+                                    node: node,
+                                    messageId: "noSingleLineIfsAsBlock",
+                                    fix(fixer) {
+                                        const sourceCode = context.getSourceCode();
 
-                                    const openingBrace = sourceCode.getFirstToken(node.consequent);
-                                    const closingBrace = sourceCode.getLastToken(node.consequent);
-                                    const firstValueToken = sourceCode.getFirstToken(node.consequent.body[0]);
-                                    const lastValueToken = sourceCode.getLastToken(node.consequent.body[0]);
+                                        const openingBrace = sourceCode.getFirstToken(node.consequent);
+                                        const closingBrace = sourceCode.getLastToken(node.consequent);
+                                        const firstValueToken = sourceCode.getFirstToken(node.consequent.body[0]);
+                                        const lastValueToken = sourceCode.getLastToken(node.consequent.body[0]);
 
-                                    return [
-                                        fixer.removeRange([openingBrace.range[0], firstValueToken.range[0]]),
-                                        fixer.removeRange([lastValueToken.range[1], closingBrace.range[1]])
-                                    ];
-                                }
-                            })
+                                        return [
+                                            fixer.removeRange([openingBrace.range[0], firstValueToken.range[0]]),
+                                            fixer.removeRange([lastValueToken.range[1], closingBrace.range[1]])
+                                        ];
+                                    }
+                                })
+                            }
                         }
                     }
                 } else if(node.alternate || node.parent.type === "IfStatement") {
@@ -43,7 +45,7 @@ module.exports = {
                             fix(fixer) {
                                 // assumes that other eslint rules will fix brace styling
                                 const sourceCode = context.getSourceCode();
-
+                                
                                 const firstValueToken = sourceCode.getFirstToken(node.consequent);
                                 const lastValueToken = sourceCode.getLastToken(node.consequent);
 
@@ -52,9 +54,9 @@ module.exports = {
                                     fixer.insertTextAfter(lastValueToken, "}")
                                 ];
                             }
-                        })
+                        })        
                     }
-
+                    
                     if(node.alternate && node.alternate.type !== "IfStatement" && node.alternate.type !== "BlockStatement") {
                         context.report({
                             node: node,
@@ -62,7 +64,7 @@ module.exports = {
                             fix(fixer) {
                                 // assumes that other eslint rules will fix brace styling
                                 const sourceCode = context.getSourceCode();
-
+                                
                                 const firstValueToken = sourceCode.getFirstToken(node.alternate);
                                 const lastValueToken = sourceCode.getLastToken(node.alternate);
 
